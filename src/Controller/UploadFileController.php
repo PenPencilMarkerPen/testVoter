ca9a0d60ba7043a5273228a6db97fce6cab7b215
Andrey Demianov <gappointgap@gmail.com>
2024-10-24 13:31:01 +0300

    test Upload

--- src/Controller/UploadFileController.php
+++ src/Controller/UploadFileController.php
@@ -4,22 +4,16 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
-use App\ApiResource\ArticleReferenceUploadApiModel;
-use Symfony\Component\HttpFoundation\File\File as FileObject;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Entity\FilesCustom;
 
 
 class UploadFileController extends AbstractController {
 
     #[Route("/upload/file", name: 'upload_file', methods: ["POST"])]
-    public function upload( Request $request, EntityManagerInterface $entityManagerInterface)
+    public function upload( Request $request, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
         $uploadedFile = $request->files->get('file');
 
