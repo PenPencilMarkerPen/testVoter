89b0c9807519da9e576449cfa8200bdac1b3b7b8
Andrey Demianov <gappointgap@gmail.com>
2024-10-25 16:07:59 +0300

    ProductContextBuilder

--- src/Serializer/ProductContextBuilder.php
+++ src/Serializer/ProductContextBuilder.php
@@ -3,25 +3,47 @@
 namespace App\Serializer;
 
 use ApiPlatform\State\SerializerContextBuilderInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use App\Entity\Product;
 
-
+#[AsDecorator('api_platform.serializer.context_builder')]
 class ProductContextBuilder implements SerializerContextBuilderInterface {
 
     public function __construct(
         private SerializerContextBuilderInterface $decorated,
         private Security $security,
+        private EntityManagerInterface $entityManagerInterface,
     )
-    {
-        
-    }
+    {}
 
     public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array
     {
         $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
-        // dump( $context);
-        return [];
+       
+        if ($context['resource_class'] !== Product::class)
+        {
+            return $context;
+        }
+
+        $id= $context['uri_variables']['id'];
+        $user = $this->security->getUser();
+        $product=$this->getProduct($id);
+
+        if ($product &&  $user===$product->getBrand()->getUsers())
+        {
+            $context['groups'][] = $normalization ? Product::PRODUCT_READ_OWNER : Product::PRODUCT_WRITE_OWNER;
+        }
+        return $context;
+    }
+
+    private function getProduct($id):Product|null
+    {
+        $product = $this->entityManagerInterface->getRepository(Product::class)->find($id);
+        
+        return $product;
     }
 }
